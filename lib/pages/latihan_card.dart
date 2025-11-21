c47f894cb07d3415626cf851f1b5c33886b1f932
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2025-11-21 20:29:36 +0700

    Menambahkan card pertama

--- lib/pages/latihan_card.dart
+++ lib/pages/latihan_card.dart
@@ -6,18 +6,27 @@ class MyCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            appBar: AppBar(
-              title: const Text("Dashboard"),
-              actions: const [],
-            ),
-            body: SingleChildScrollView(
-              child: Container(
-                padding: const EdgeInsets.all(20.0),
-                child: const Column(
-                  children: [],
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: const Column(
+            children: [
+              Card(
+                color: Colors.red,
+                child: Text(
+                  "Card with color",
+                  style: TextStyle(fontSize: 16.0),
                 ),
               ),
-            ),
-          );();
+            ],
+          ),
+        ),
+      ),
+    );
   }
-} 
+}
