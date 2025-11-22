80ad82955f4888ecb36006deaa522f479d889315
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2025-11-22 20:32:29 +0700

    Menambahkan new tittle pada main

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "NIM : 1123150141, ilhamMaulana",
+      title: "Week8-Latihan Card&Container",
       theme: ThemeData(
         useMaterial3: true,
       ),
