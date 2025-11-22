ee1fd7d9d690ff1f62e2c04178d48d5c6c53d010
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2025-11-22 22:02:23 +0700

    step 5 wrapping ke padding

--- lib/pages/latihan_card02.dart
+++ lib/pages/latihan_card02.dart
@@ -14,39 +14,42 @@ class MyCard02 extends StatelessWidget {
               child: Container(
                 padding: const EdgeInsets.all(20.0),
                 child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        children: [
-
-                          Container(
-                            decoration: BoxDecoration(shape: BoxShape.circle),
-                            child: CircleAvatar(
-                              radius: 60,
-                              backgroundImage: AssetImage("assets/images/bott.jpg"),
-                            ),
-                          ),
-                      
-                          Card(
-                            elevation: 8,
-                            shadowColor: Colors.red,
-                            clipBehavior: Clip.antiAlias,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                  begin: Alignment.topRight,
-                                  end: Alignment.bottomLeft,
-                                  colors: [Colors.blue, Colors.red],
-                                ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                    
+                            Container(
+                              decoration: BoxDecoration(shape: BoxShape.circle),
+                              child: CircleAvatar(
+                                radius: 60,
+                                backgroundImage: AssetImage("assets/images/bott.jpg"),
                               ),
-                              child: Text("text", style: TextStyle(fontSize: 20.0)),
                             ),
-                          ),
-                      
-                        ],
-                      ),
-                    ],
+                        
+                            Card(
+                              elevation: 8,
+                              shadowColor: Colors.red,
+                              clipBehavior: Clip.antiAlias,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topRight,
+                                    end: Alignment.bottomLeft,
+                                    colors: [Colors.blue, Colors.red],
+                                  ),
+                                ),
+                                child: Text("text", style: TextStyle(fontSize: 20.0)),
+                              ),
+                            ),
+                        
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
