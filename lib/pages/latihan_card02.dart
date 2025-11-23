5c38c7fcf56c056209a67c5f5c856f4c793ccc95
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2025-11-23 20:27:06 +0700

    step 9 Menambahkan Role SubText

--- lib/pages/latihan_card02.dart
+++ lib/pages/latihan_card02.dart
@@ -29,38 +29,56 @@ class MyCard02 extends StatelessWidget {
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border: Border.all(color: Colors.white, width: 4),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black.withValues(alpha: 0.2),
-                                    blurRadius: 10,
-                                    offset: Offset(0, 5),
-                                  ),
-                                ],
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(color: Colors.white, width: 4),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black.withValues(alpha: 0.2),
+                                      blurRadius: 10,
+                                      offset: Offset(0, 5),
+                                    ),
+                                  ],
+                            ),
+                            
+                            child: CircleAvatar(
+                              radius: 60,
+                              backgroundImage: AssetImage("assets/images/bott.jpg"),
+                            ),
                           ),
-                          
-                          child: CircleAvatar(
-                            radius: 60,
-                            backgroundImage: AssetImage("assets/images/bott.jpg"),
+                      
+                          SizedBox(height: 10),
+                          Text(
+                            "Muhammad Ilham Maulana",
+                            style: TextStyle(
+                              fontSize: 26.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                        
-                        SizedBox(height: 10),
-                        Text(
-                          "Muhammad Ilham Maulana",
-                          style: TextStyle(
-                            fontSize: 26.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                      
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const Text(
+                              'UI/UX Designer',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
                           ),
-                        ),
-                      ],
+                      
+                        ],
+                      ),
                     ),
                   ),
                 ),
